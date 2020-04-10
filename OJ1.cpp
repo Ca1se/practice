98e61ae8b82230542f742162dd747bce6e5a64f4
Cardonika <wzhtongtianzhe@sina.com>
2020-04-10 22:31:27 +0800

    安妮的第一本字典

---
+++ OJ1.cpp
@@ -0,0 +1,33 @@
+//集合:set
+/*安迪的第一个字典
+输入一个文本,找出所有不同的单词(连续的字母序列),按字典序从小到大输出,单词不区分大小写*/
+
+
+#include<iostream>
+#include<string>
+#include<set>
+#include<sstream>
+
+using namespace std;
+
+set<string> dict;
+
+int main()
+{
+    freopen("text.in","r",stdin);
+    freopen("text.out","w",stdout);
+    string temp,buf;
+    while (cin >> temp)
+    {
+        for(int r=0;r<temp.size();r++)
+            if(isalpha(temp[r])==0)
+                temp[r]=0;
+            else if(isalpha(temp[r])==1)
+                temp[r]+=32;
+        stringstream ss(temp);
+        while (ss >> buf)   dict.insert(buf);
+    }
+    for(set<string>::iterator it = dict.begin();it!=dict.end();it++)
+        cout << *it << endl;
+}
+
