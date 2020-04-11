e6566aba41adb6e4bcb329bbac54f29fea19b24c
caseshot <blame773@gmail.com>
2020-04-11 15:18:45 +0800

    安妮的第一本字典

--- OJ1.cpp
+++ OJ1.cpp
@@ -1,7 +1,7 @@
 //集合:set
 /*安迪的第一个字典
 输入一个文本,找出所有不同的单词(连续的字母序列),按字典序从小到大输出,单词不区分大小写*/
-
+/*
 #include<iostream>
 #include<string>
 #include<set>
@@ -13,20 +13,42 @@ set<string> dict;
 
 int main()
 {
-    freopen("text.in","r",stdin);
-    freopen("text.out","w",stdout);
+
     string temp,buf;
     while (cin >> temp)
     {
         for(int r=0;r<temp.size();r++)
-            if(isalpha(temp[r])==0)
-                temp[r]=0;
-            else if(isalpha(temp[r])==1)
-                temp[r]+=32;
+            if(isalpha(temp[r]))    temp[r]=tolower(temp[r]); else temp[r]=' ';
         stringstream ss(temp);
         while (ss >> buf)   dict.insert(buf);
     }
     for(set<string>::iterator it = dict.begin();it!=dict.end();it++)
         cout << *it << endl;
-}
+    return 0;
+}*/
 
+#include<iostream>
+#include<string>
+#include<set>
+#include<sstream>
+using namespace std;
+
+set<string> dict;
+
+int main()
+{
+    string s,buf;
+    while (cin >> s)
+    {
+        for(int i=0;i<s.length();i++)
+            if(isalpha(s[i]))   s[i]=tolower(s[i]); else  s[i]=' ';
+        stringstream ss(s);
+        while (ss >> buf)
+        {
+            dict.insert(buf);
+        }
+    }
+    for(set<string>::iterator it=dict.begin();it!=dict.end();it++)
+        cout << *it << endl;
+    return 0;
+}
