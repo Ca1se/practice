854716672016352bc42769575722c5129f374568
Cardonika <wzhtongtianzhe@sina.com>
2020-05-26 00:01:59 +0800

    2020.5.26

--- 2020.5.14.3.cpp
+++ 2020.5.14.3.cpp
@@ -1,25 +1,26 @@
 #include<iostream>
-#include<set>
+#include<string>
 #include<sstream>
+#include<algorithm>
 using namespace std;
-int main(){
+bool cmp(int x, int y) {
+    return x < y;
+}
+int main() {
     ios::sync_with_stdio(false);
-    set<int> t;
     string a;
     while (cin >> a)
     {
-        int temp;
-        for(int i=0;i<a.size();i++){
-            if(a[i]=='5')   a[i]=' ';
+        int number[1000], cnt = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] == '5')   a[i] = ' ';
         }
         stringstream ss(a);
-        while (ss >> temp)
-        {
-            t.insert(temp);    
-        }
-        for(set<int>::iterator it=t.begin();it!=t.end();it++){
-            if(it!=t.begin())   cout << " ";
-            cout << *it;
+        while (ss >> number[cnt++]);//cnt循环完毕后=数的个数+2
+        sort(number, number + cnt-1, cmp);
+        for (int i = 0; i < cnt-1; i++) {
+            if (i)   cout << " ";
+            cout << number[i];
         }
         cout << endl;
     }
