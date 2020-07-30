3fea92d2be56a0406bf86b70aa4491e73c797f63
caseshot <blame773@gmail.com>
2020-07-30 17:48:23 +0800

    表达式树

---
+++ Uva572.cpp
@@ -0,0 +1,42 @@
+#include<cstdio>
+#include<cstring>
+const int maxn = 100 + 5;
+char pic[maxn][maxn];
+int mark[maxn][maxn], m, n;
+
+void dfs(int i, int j){
+    if((i < 0 || i >= m) || (j < 0 || j >= n))  return;//超出范围
+    if(mark[i][j] != 0 || pic[i][j] == '*') return;
+    mark[i][j] = 1;
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i + 1, j);
+    dfs(i, j - 1);
+    dfs(i - 1, j + 1);
+    dfs(i + 1, j + 1);
+    dfs(i + 1, j - 1);
+    dfs(i - 1, j - 1);
+    return;
+}
+
+int main(){
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w+", stdout);
+    while (scanf("%d%d", &m, &n) == 2 && m && n){
+        int cnt = 0;
+        memset(mark, 0, sizeof(mark));
+        for(int i = 0; i < m; i++){
+            scanf("%s", pic[i]);
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mark[i][j] == 0 && pic[i][j] == '@'){
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        printf("%d\n", cnt);
+    }
+    return 0;
+}
---
+++ Uva679.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int tree[1 << 20];
+int main(){
+    //freopen("test.in", "r", stdin);
+    //freopen("test.out", "w+", stdout);
+    int D,I; // 深度D, 小球个数I
+    for(int i = 0; i < 1000 && cin >> D >> I; i++){
+        memset(tree, 0, sizeof(tree));
+        int step;
+        for(int j = 0; j < I; j++){
+            step = 1;
+            while (step < (1 << D))
+            {
+                if(tree[step] == 0){
+                    tree[step] = 1;
+                    step = 2 * step;
+                }else{
+                    tree[step] = 0;
+                    step = 2 * step + 1;
+                }
+            }
+        }
+        cout << step / 2 << endl;
+    }
+}
