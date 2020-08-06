3451b25797fde945abd79feb68178889380faed3
caseshot <blame773@gmail.com>
2020-08-06 20:22:36 +0800

    Round 661

---
+++ 1399A.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+int test, n, a[50 + 5], mark[50 + 5];
+bool solve() {
+    int cnt = 0;
+    memset(mark, 0, sizeof(mark));
+    sort(a, a + n);
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(!mark[i] && !mark[j] && abs(a[i] - a[j]) <= 1){
+                cnt++;
+                if(cnt == n - 1)    return true;
+                if(a[i] <= a[j]){
+                    break;
+                }else{
+                    mark[j] = 1;
+                }
+            }
+        }
+    }
+    return false;
+}
+int main() {
+    freopen("test.in", "r", stdin);
+    cin >> test;
+    for(int i = 0; i < test; i++){
+        cin >> n;
+        for(int j = 0; j < n; j++){
+            cin >> a[j];
+        }
+        if(solve() || n == 1){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
