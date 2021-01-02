6ffa01cefc8fe082e5acc93c9e62cca8840cfba3
Ca1seMonika <irstCa1se@outlook.com>
2021-01-02 11:55:34 +0000

    二叉树的编号

--- Uva679.cpp
+++ Uva679.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+/*
 int tree[1 << 20];
+
+//对于编号为n的节点, 它的左子节点编号为n * 2, 它的右子节点编号为n * 2 + 1
 
 int main(){
     //freopen("test.in", "r", stdin);
@@ -24,6 +27,29 @@ int main(){
             }
         }
         cout << step / 2 << endl;
+    }
+    return 0;
+}*/
+
+//直接模拟最后一个球
+//对于经过某节点的第n个球, 如果n是奇数, 则球会向左子节点走且是第(n + 1) / 2个经过左子节点的,
+//如果n是偶数, 则球会向右子节点走且是第n / 2个经过右子节点的
+int main() {
+    int D, I, n;
+    while(scanf("%d", &n) == 1 && n != -1) {
+        for(int j = 0; j < n && scanf("%d%d", &D, &I); j++) {
+            int k = 1;
+            for(int i = 0; i < D - 1; i++) {
+                if(I % 2) {
+                    k *= 2;
+                    I = (I + 1) / 2;
+                }else {
+                    k = k * 2 + 1;
+                    I /= 2;
+                }
+            }
+            printf("%d\n", k);
+        }
     }
     return 0;
 }
