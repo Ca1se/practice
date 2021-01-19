a1d30cdf43286c4b1fb50eff90b6bb22f9cdaa71
Ca1seMonika <irstCa1se@outlook.com>
2021-01-19 21:44:35 +0800

    线程传参问题

---
+++ MultiThreadtest1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<thread>
+
+void foo1(const std::string s, int i) {
+    //so something
+}
+
+void foo(int param) {
+    char buf[1024];
+    sprintf(buf, "%d", param);
+    //std::thread th(foo1, buf, param); 新线程开始时, buf与param将被拷贝到新线程的内存空间中,
+    //但buf为一个指针类型, 以下使用了th.detach(), 主线程不会等待新线程结束。则buf指向的内存空间可能在
+    //其中的内容被转换成一个std::string类型前就被释放掉了, 导致未定义行为
+    std::thread th(foo1, std::string(buf), param);
+    th.detach();
+}
+
+
+int main() {
+    foo(3);
+    return 0;
+}
