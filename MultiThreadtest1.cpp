622ec8f2f3ef0f2d1c101832bdfda3ac3cc07983
caseshot <blame773@gmail.com>
2021-01-19 22:10:08 +0800

    多线程函数传参问题

--- MultiThreadtest1.cpp
+++ MultiThreadtest1.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<thread>
 
-void foo1(const std::string s, int i) {
-    //so something
+void PassByReference(std::string& data) {
+    //do something
+}
+
+void PassByValue(const std::string s, int i) {
+    //do something
 }
 
 void foo(int param) {
@@ -11,8 +15,14 @@ void foo(int param) {
     //std::thread th(foo1, buf, param); 新线程开始时, buf与param将被拷贝到新线程的内存空间中,
     //但buf为一个指针类型, 以下使用了th.detach(), 主线程不会等待新线程结束。则buf指向的内存空间可能在
     //其中的内容被转换成一个std::string类型前就被释放掉了, 导致未定义行为
-    std::thread th(foo1, std::string(buf), param);
+    std::thread th(PassByValue, std::string(buf), param);
     th.detach();
+}
+
+void foo(std::string& rs) {
+    //如果传入的函数期望函数参数以引用方式传递, 使用std::ref()
+    std::thread th(PassByReference, std::ref(rs));
+    th.join();
 }
 
 
