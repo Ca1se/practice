92270a12b62249580a30f38adad88e76651c07e2
Wu Ye <63452370+Ca1seMonika@users.noreply.github.com>
2021-02-24 18:15:04 +0800

    简单

--- leetcode832.cpp
+++ leetcode832.cpp
@@ -4,9 +4,9 @@ public:
         int n = A.size(), m = A[0].size();
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m / 2; j++) {
-                if(A[i][j] == A[i][m - j - 1])    A[i][j] = A[i][m - j - 1] = !A[i][j];
+                if(A[i][j] == A[i][m - j - 1])    A[i][j] = A[i][m - j - 1] ^= 1;
             }
-            if(m % 2)   A[i][m / 2] = !A[i][m / 2];
+            if(m % 2)   A[i][m / 2] ^= 1;
         }
         return A;
     }
