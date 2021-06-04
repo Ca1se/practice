5c51d80db67e0a4d65d2b65aa8a0ff93bf79f5da
Ca1se <irstCa1se@outlook.com>
2021-06-04 23:56:56 +0800

    数值分析大作业

---
+++ term_project/src/main.rs
@@ -0,0 +1,72 @@
+type float_44 = [[f64; 4]; 4];
+type float_14 = [f64; 4];
+type int_44 = [[i32; 4]; 4];
+fn calculate_matrix_lu(matrix_a: &float_44, 
+    matrix_l: &mut float_44, matrix_u: &mut float_44) {
+    
+    for r in 0..4 {
+        for i in r..4 {
+            let mut sum = 0.0;
+            for k in 0..r {
+                sum = sum + matrix_l[r][k] * matrix_u[k][i];
+            }
+            matrix_u[r][i] = matrix_a[r][i] - sum;
+        }
+
+        for i in r..4 {
+            let mut sum = 0.0;
+            for k in 0..r {
+                sum = sum + matrix_l[i][k] * matrix_u[k][r];
+            }
+            matrix_l[i][r] = (matrix_a[i][r] - sum) / matrix_u[r][r];
+        }
+    }         
+}
+
+fn convert_f64_matrix(ori: &int_44) -> float_44 {
+    let mut a = [[0.0; 4]; 4];
+    for i in 0..4 {
+        for j in 0..4 {
+            a[i][j] = ori[i][j] as f64;
+        };
+    } 
+    a
+}
+
+fn calculate_matrix_x(matrix_l: &mut float_44, 
+    matrix_u: &mut float_44, matrix_b: &float_14) -> float_14{
+    let mut m_x = [0.0; 4];
+    let mut m_y = [0.0; 4];
+    for i in 0..4 {
+        let mut sum = 0.0;
+        for k in 0..i {
+            sum = sum + matrix_l[i][k] * m_y[k];
+        }
+        m_y[i] = matrix_b[i] - sum;
+    }
+    
+    for i in (0..4).rev() {
+        let mut sum = 0.0;
+        for k in (i + 1)..4 {
+            sum = sum + matrix_u[i][k] * m_x[k];
+        }
+        m_x[i] = (m_y[i] - sum) / matrix_u[i][i];
+    }
+    m_x
+}
+
+
+fn main() {
+    let origin_a= [[11, 20, 20, 50],
+                    [50, 40, 10, 10],
+                    [30, 10, 40, 20],
+                    [20, 40, 40, 40]];
+    let matrix_a = convert_f64_matrix(&origin_a);
+    let mut matrix_u = [[0.0; 4]; 4];
+    let mut matrix_l = matrix_u.clone(); 
+    let matrix_b = [200.0, 250.0, 210.0, 340.0];
+    calculate_matrix_lu(&matrix_a, &mut matrix_l, &mut matrix_u);
+    let matrix_x = calculate_matrix_x(&mut matrix_l, &mut matrix_u, &matrix_b);
+    println!("matrix X:\n{:?}T", matrix_x);
+    
+}
---
+++ term_project/target/debug/deps/term_project-2014d3e60c878501.d
@@ -0,0 +1,5 @@
+/home/ca1se/Documents/practice/term_project/target/debug/deps/term_project-2014d3e60c878501.rmeta: src/main.rs
+
+/home/ca1se/Documents/practice/term_project/target/debug/deps/term_project-2014d3e60c878501.d: src/main.rs
+
+src/main.rs:
