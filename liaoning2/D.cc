b75e98b55a0decc6907335301f3615b9e3a8942e
caseshot <blame773@gmail.com>
2021-10-25 17:12:49 +0800

    2021.10.25

---
+++ liaoning2/D.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	long long n, m, x, y;
+	scanf("%d", &t);
+	while(t--) {
+		scanf("%lld%lld%lld%lld", &n, &m, &x, &y);
+		if(n != 1 && m != 1) {
+    		n--, m--;
+    		if(y >= 2 * x) printf("%lld\n", (n + m) * x);
+    		else {
+    			long long ans = 0;
+    			ans += min(n, m) * y;
+    			int last = abs(m - n);
+    			if(y < x) ans += (last / 2) * y * 2 + (last - last / 2 * 2) * x;
+    			else ans += last * x;
+    			printf("%lld\n", ans);
+    		}
+		}else {
+			n--, m--;
+			printf("%lld\n", max(n, m) * x);
+		}
+	}
+	return 0;
+}
---
+++ liaoning2/F.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+char arr[200];
+char ans[200];
+int main() {
+	scanf("%s", arr);
+	int diff = 'a' - 'A';
+	if(arr[0] >= 'a' && arr[0] <= 'z') {
+		bool j = true;
+		ans[0] = arr[0] - diff;
+		for(int i = 1; i < strlen(arr); i++) {
+			if(arr[i] >= 'a' && arr[i] <= 'z') {
+				j = false;
+				break;
+			}else {
+				ans[i] = arr[i] + diff;
+			}
+			
+		}
+		if(j) {
+			printf("%s\n", ans);
+		}else {
+			printf("%s\n", arr);
+		}
+	}else {
+		bool j = true;
+		ans[0] = arr[0] + diff;
+		for(int i = 1; i < strlen(arr); i++) {
+			if(arr[i] >= 'a' && arr[i] <= 'z') {
+				j = false;
+				break;
+			}else {
+				ans[i] = arr[i] + diff;
+			}
+		}
+		if(j) {
+			printf("%s\n", ans);
+		}else {
+			printf("%s\n", arr);
+		}
+	}
+	return 0;
+}
