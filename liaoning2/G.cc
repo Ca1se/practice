15b62482d3a88a42142e6b10112d3ecc76c85caf
Ca1se <irstCa1se@outlook.com>
2021-10-25 17:31:47 +0800

    2021.10.25

--- liaoning2/G.cc
+++ liaoning2/G.cc
@@ -2,34 +2,34 @@
 using namespace std;
 char arr[200];
 int main() {
-	scanf("%s", arr);
-	int len = strlen(arr);
-	if(len > 19) {
-		printf("BigInteger\n");
-	}else if(len > 10) {
-		if(len == 19) {
-			long long a;
-			char tmp[20] = {};
-			sscanf(arr, "%lld", &a);
-			sprintf(tmp, "%lld", a);
-			if(strcmp(arr, tmp)) printf("BigInteger\n");
-			else printf("long\n");
-		}else printf("long\n");
-	}else if(len > 5) {
-		int a;
-		sscanf(arr, "%d", &a);
-		if(a > 0) printf("int\n");
-		else printf("long\n");
-	}else if(len > 3) {
-		short a;
-		sscanf(arr, "%d", &a);
-		if(a > 0) printf("short\n");
-		else printf("int\n");
-	}else {
-		signed char a;
-		sscanf(arr, "%d", &a);
-		if(a > 0) printf("byte\n");
-		else printf("short\n");
-	}
-	return 0;
+    scanf("%s", arr);
+    int len = strlen(arr);
+    if(len > 19) {
+        printf("BigInteger\n");
+    }else if(len > 10) {
+        if(len == 19) {
+            long long a;
+            char tmp[20] = {};
+            sscanf(arr, "%lld", &a);
+            sprintf(tmp, "%lld", a);
+            if(strcmp(arr, tmp)) printf("BigInteger\n");
+            else printf("long\n");
+        }else printf("long\n");
+    }else if(len > 5) {
+        int a;
+        sscanf(arr, "%d", &a);
+        if(a > 0) printf("int\n");
+        else printf("long\n");
+    }else if(len > 3) {
+        short a;
+        sscanf(arr, "%d", &a);
+        if(a > 0) printf("short\n");
+        else printf("int\n");
+    }else {
+        signed char a;
+        sscanf(arr, "%d", &a);
+        if(a > 0) printf("byte\n");
+        else printf("short\n");
+    }
+    return 0;
 }
--- liaoning2/M.cc
+++ liaoning2/M.cc
@@ -5,45 +5,45 @@ int c[maxn];
 int topo[maxn], t, n = 1;
 bool G[maxn][maxn];
 bool dfs(int u) {
-	c[u] = -1;
-	for(int v = 1; v < n; v++) if(G[u][v]) {
-		if(c[v] < 0) return false;
-		else if(!c[v] && !dfs(v)) return false;
-	}
-	c[u] = 1; topo[--t] = u;
-	return true;
+    c[u] = -1;
+    for(int v = 1; v < n; v++) if(G[u][v]) {
+        if(c[v] < 0) return false;
+        else if(!c[v] && !dfs(v)) return false;
+    }
+    c[u] = 1; topo[--t] = u;
+    return true;
 }
 bool toposort() {
-	t = n;
-	memset(c, 0, sizeof c);
-	for(int u = 1; u < n; u++) {
-		if(!c[u]) {
-			if(!dfs(u)) return false;
-		}
-	}
-	return true;
+    t = n;
+    memset(c, 0, sizeof c);
+    for(int u = 1; u < n; u++) {
+        if(!c[u]) {
+            if(!dfs(u)) return false;
+        }
+    }
+    return true;
 }
 map<char, int> mp;
 map<int, char> mpp;
 char arr[10];
 int main() {
-	int r;
-	scanf("%d", &r);
-	for(int i = 0; i < r; i++) {
-		scanf("%s", arr);
-		if(mp[arr[0]] == 0) mp[arr[0]] = n, mpp[n++] = arr[0];
-		if(mp[arr[2]] == 0) mp[arr[2]] = n, mpp[n++] = arr[2];
-		if(mp[arr[3]] == 0) mp[arr[3]] = n, mpp[n++] = arr[3];
-		G[mp[arr[0]]][mp[arr[2]]] = true;
-		G[mp[arr[3]]][mp[arr[0]]] = true;
-	}
-	if(toposort()) {
-    	for(int i = n - 1; i >= 1; i--) {
-    		printf("%c", mpp[topo[i]]);
-    	}
-    	printf("\n");
-	}else {
-		printf("No Answer\n");
-	}
-	return 0;
+    int r;
+    scanf("%d", &r);
+    for(int i = 0; i < r; i++) {
+        scanf("%s", arr);
+        if(mp[arr[0]] == 0) mp[arr[0]] = n, mpp[n++] = arr[0];
+        if(mp[arr[2]] == 0) mp[arr[2]] = n, mpp[n++] = arr[2];
+        if(mp[arr[3]] == 0) mp[arr[3]] = n, mpp[n++] = arr[3];
+        G[mp[arr[0]]][mp[arr[2]]] = true;
+        G[mp[arr[3]]][mp[arr[0]]] = true;
+    }
+    if(toposort()) {
+        for(int i = n - 1; i >= 1; i--) {
+            printf("%c", mpp[topo[i]]);
+        }
+        printf("\n");
+    }else {
+        printf("No Answer\n");
+    }
+    return 0;
 }
