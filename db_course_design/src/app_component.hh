e8a8c4bf223c72b554ba217a04f768563a462b9d
caseshot <blame773@gmail.com>
2021-12-22 17:03:51 +0800

    2021.12.22

---
+++ db_course_design/src/app_component.hh
@@ -0,0 +1,41 @@
+#ifndef _APP_COMPONENT_HH_
+#define _APP_COMPONENT_HH_
+
+#include <memory>
+#include <oatpp-1.3.0/oatpp/oatpp/network/ConnectionHandler.hpp>
+#include <oatpp/network/Address.hpp>
+#include <oatpp/network/ConnectionProvider.hpp>
+#include <oatpp/core/macro/component.hpp>
+#include <oatpp/web/server/HttpConnectionHandler.hpp>
+#include <oatpp/web/server/HttpRouter.hpp>
+#include <oatpp/network/tcp/server/ConnectionProvider.hpp>
+#include "ui/ui_component.hh"
+
+class AppComponent {
+public:
+    UIComponent ui_component;
+
+    /**
+    * create http router component
+    */
+    OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router)([] {
+        return oatpp::web::server::HttpRouter::createShared();
+    }());
+
+    /**
+    * create tcp connection provider component
+    */
+    OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, serverConnectionProvider)([] {
+        return oatpp::network::tcp::server::ConnectionProvider::createShared({"0.0.0.0", 8000, oatpp::network::Address::IP_4});
+    }());
+
+    /**
+    * create http connection handler
+    */
+    OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler)([] {
+        OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+        return oatpp::web::server::HttpConnectionHandler::createShared(router);
+    }());
+};
+
+#endif
---
+++ db_course_design/src/main.cc
@@ -0,0 +1,36 @@
+#include <memory>
+#include <oatpp/network/ConnectionHandler.hpp>
+#include <oatpp/network/ConnectionProvider.hpp>
+#include <oatpp/web/server/HttpRouter.hpp>
+#include <oatpp/core/macro/component.hpp>
+#include <oatpp/core/base/Environment.hpp>
+#include <oatpp/network/Server.hpp>
+#include "app_component.hh"
+#include "ui/ui_controller.hh"
+
+inline void run() {
+    AppComponent component;
+
+    OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+    router->addController(UIController::createShared());
+
+    OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+
+    OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+
+    oatpp::network::Server server(connectionProvider,
+            connectionHandler);
+
+    OATPP_LOGD("Server", "Running on port %s", connectionProvider->getProperty("port").toString()->c_str());
+
+    server.run();
+}
+
+int main() {
+    oatpp::base::Environment::init();
+
+    run();
+
+    oatpp::base::Environment::destroy();
+    return 0;
+}
