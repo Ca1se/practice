f76f6a0dd1cf36d8d42b42313a4aa0ce6180e269
Ca1se <irstCa1se@outlook.com>
2022-04-01 10:29:46 +0800

    2022.4.1

--- opengl-practice/gc_homework1/src/shader/fragment.glsl
+++ opengl-practice/gc_homework1/src/shader/fragment.glsl
@@ -1,8 +1,6 @@
 #version 330 core
 out vec4 fragment_color;
 
-uniform float green;
-
 void main() {
-    fragment_color = vec4(green, green, green, green);
+    fragment_color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
