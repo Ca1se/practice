cdfbfe8d26e9ea2d6988110dd99f87abdbd91d35
caseshot <blame773@gmail.com>
2022-04-22 16:51:32 +0800

    4.22

--- term_project/web_term_project/site/script/designer.js
+++ term_project/web_term_project/site/script/designer.js
@@ -1,18 +1,47 @@
 // @ts-check
 
-import { addShader, drawLines } from "./utils";
-import { kLineVsSource, kLineFsSource} from './shader_source';
+import { drawLines } from './utils.js';
+import { Point, Rectangle, Shape } from './typedef.js';
 
-function main() {
+
+
+function drawBackground() {
     /** @type {HTMLCanvasElement} */
-    const canvas = document.querySelector('.glcanvas');
-    const gl = canvas.getContext('webgl2');
+    const canvas = document.querySelector('#back-canvas');
+    const ctx = canvas.getContext('2d');
 
-    addShader(gl, 'line', kLineVsSource, kLineFsSource);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    /** @type {import("./utils").LineInfo} */
-    const line = {
-    };
-
-    drawLines(gl, )
+    /** @type {{ start: Point, end: Point }[]} */
+    let arr = [];
+    let arr1 = [];
+    let interval = 30;
+    for(let i = 0; i < canvas.height; i += interval) {
+        let t;
+        if(i % 90 != 0) { t = arr; }
+        else { t = arr1; }
+        t.push({
+            start: new Point(0, i),
+            end:   new Point(canvas.width, i),
+        });
+    }
+    for(let i = 0; i < canvas.width; i += interval) {
+        let t;
+        if(i % 90 != 0) { t = arr; }
+        else { t = arr1; }
+        t.push({
+            start: new Point(i, 0),
+            end:   new Point(i, canvas.height)
+        });
+    }
+    drawLines(ctx, arr, 0.08, 'black');
+    drawLines(ctx, arr1, 0.3, 'black');
 }
+
+$(function main() {
+    drawBackground();
+
+    const rect = new Rectangle(new Point(100, 100), 100, 50, '#000000');
+    rect.show('container');
+})
--- term_project/web_term_project/site/script/typedef.js
+++ term_project/web_term_project/site/script/typedef.js
@@ -1,5 +1,7 @@
 // ts-check
 
+import { Manage } from './utils.js';
+
 class Point {
     constructor(x = 0, y = 0) {
         this.data = [x, y];
@@ -12,32 +14,204 @@ class Point {
     get y() {
         return this.data[1];
     }
+
+    /**
+     * @param {number | Point} other 
+     */
+    add(other) {
+        if(other instanceof Point) {
+            return new Point(this.x + other.x, this.y + other.y);
+        }
+
+        return new Point(this.x + other, this.y + other);
+    }
+
+    /**
+     * @param {number | Point} other 
+     */
+    sub(other) {
+        if(other instanceof Point) {
+            return new Point(this.x - other.x, this.y - other.y);
+        }
+
+        return new Point(this.x - other, this.y - other);
+    }
+
+    /**
+     * @param {number} other 
+     */
+    mul(other) {
+        return new Point(this.x * other, this.y * other);
+    }
+
+    /**
+     * @param {number} other 
+     */
+    div(other) {
+        return new Point(this.x / other, this.y / other);
+    }
 }
 
-class RGBA {
-    constructor(r = 0, g = 0, b = 0, a = 1) {
-        this.data = [r, g, b, a];
+
+
+class Shape {
+    /**
+     * @param {Point} origin 
+     * @param {string} color
+     */
+    constructor(origin, color) {
+        this.origin = origin,
+        this.color = color;
+
+        // this.up = new Point(Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+        this.up = new Point(0, -1);
+        this.id = parseInt(Math.random().toString().substring(2, 6) + Date.now().toString().substring(7));
+        this.showed = false;
+        this.belong = '';
     }
 
-    get r() {
-        return this.data[0];
+    /**
+     * @param {string} container
+     * @param {Point} xy
+     * @param {number} width
+     * @param {number} height
+     */
+    show(container, xy, width, height) {
+        const item = $(`<canvas id="${'i' + this.id}" width="${width}" height="${height}"></canvas>`).css(
+            { 
+                'position': 'absolute',
+                'top': xy.y,
+                'left': xy.x
+            }
+        );
+        $(`#${container}`).append(item);
+        this.belong = container;
+        this.showed = true;
+
+        /** @type {HTMLCanvasElement} */
+        const html = item[0];
+        this.ctx = html.getContext('2d');
     }
 
-    get g() {
-        return this.data[1];
+    remove() {
+        $(`#${this.id}`).remove();
+        this.belong = '';
+        this.showed = false;
     }
 
-    get b() {
-        return this.data[2];
+    /**
+     * @param {Point} min_xy
+     * @param {boolean} fill
+     */
+    drawShape(min_xy, fill) { throw 'No implementation'; }
+
+    /**
+     * @param {boolean} enable 
+     */
+    setZoomFrame(enable) { throw 'No implementation'; }
+
+
+}
+
+class Rectangle extends Shape {
+    /**
+     * @param {Point} origin 
+     * @param {number} width 
+     * @param {number} height 
+     * @param {string} color
+     */
+    constructor(origin, width, height, color) {
+        super(origin, color);
+        this.width = width;
+        this.height = height;
     }
 
-    get a() {
-        return this.data[3];
+    /**
+     * @param {Point} origin
+     * @returns {Point[]}
+     */
+    getVertice(origin) {
+        const v_offset = this.up.mul(this.height / 2);
+        const h_offset = (new Point(this.up.y, -this.up.x)).mul(this.width / 2);
+
+        return [
+            origin.add(v_offset).add(h_offset),
+            origin.add(v_offset).sub(h_offset),
+            origin.sub(v_offset).sub(h_offset),
+            origin.sub(v_offset).add(h_offset)
+        ];
+    }
+
+    /**
+     * @param {string} container
+     */
+    show(container) {
+        const points = this.getVertice(this.origin);
+        const min_xy = new Point(Math.min(points[0].x, points[1].x, points[2].x, points[3].x) - 10,
+                                 Math.min(points[0].y, points[1].y, points[2].y, points[3].y) - 10);
+        const max_xy = new Point(Math.max(points[0].x, points[1].x, points[2].x, points[3].x) + 10,
+                                 Math.max(points[0].y, points[1].y, points[2].y, points[3].y) + 10);
+        Manage.shape_map.set(this.id, this);
+        super.show(container, min_xy, max_xy.x - min_xy.x, max_xy.y - min_xy.y);
+        this.drawShape(min_xy, false);
+    }
+
+    remove() {
+        Manage.shape_map.delete(this.id);
+        super.remove();
+    }
+
+    /**
+     * @param {Point} min_xy
+     * @param {boolean} fill
+     */
+    drawShape(min_xy, fill) {
+        const ori = this.origin.sub(min_xy);
+        const points = this.getVertice(ori);
+        const ctx = this.ctx;
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(points[3].x, points[3].y);
+        for(let i = 0; i < points.length; i++) {
+            ctx.lineTo(points[i].x, points[i].y);
+        }
+        ctx.closePath();
+        if(fill) {
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }else {
+            ctx.strokeStyle = this.color;
+            ctx.stroke();
+        }
+    }
+
+    /**
+     * @param {boolean} enable 
+     */
+    setZoomFrame(enable) { throw 'No implementation'; }
+
+}
+
+class Circle extends Shape {
+    /**
+     * @param {Point} origin 
+     * @param {number} radius 
+     * @param {string} color
+     */
+    constructor(origin, radius, color) {
+        super(origin, color);
+        this.radius = radius;
     }
 }
 
+class Triangle extends Shape {
+
+}
 
 export {
     Point,
-    RGBA
+    Shape,
+    Rectangle,
+    Circle,
+    Triangle
 }
