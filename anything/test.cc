f31b23a3225e58fe83c17f86a0a4305aa5e46b35
caseshot <blame773@gmail.com>
2022-05-01 12:51:05 +0800

    5.1

--- anything/test.cc
+++ anything/test.cc
@@ -1,13 +1,43 @@
-#include <iostream>
-#include <string>
+#include <cstdio>
+#include <functional>
+#include <vector>
+#include <queue>
 using namespace std;
 
+const int maxn = 500 + 5;
+vector<int> edge[maxn];
+vector<int> res;
+priority_queue<int, vector<int>, greater<int>> q;
+int ind[maxn];
+
 int main() {
-    int a[3] = {10, 20, 30};
-
-    int (&b)[3] = a;
-
-    std::cout << (sizeof b) << std::endl;
-    
+    int n, m, u, v;
+    scanf("%d%d", &n, &m);
+    for(int i = 0; i < m; i++) {
+        scanf("%d%d", &u, &v);
+        edge[u].push_back(v);
+        ind[v]++;
+    }
+    for(int i = 0; i < n; i++) {
+        if(ind[i] == 0) {
+            q.push(i);
+        }
+    }
+    while(!q.empty()) {
+        int p = q.top(); q.pop();
+        res.push_back(p);
+        for(auto it: edge[p]) {
+            ind[it]--;
+            if(ind[it] == 0) q.push(it);
+        }
+    }
+    if(res.size() == n) {
+        for(int i = 0; i < res.size(); i++) {
+            printf("%d ", res[i]);
+        }
+        puts("");
+    }else {
+        puts("false");
+    }
     return 0;
 }
