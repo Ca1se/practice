ab1428aaf77c4da4d602c271dbc29b77c685f188
caseshot <blame773@gmail.com>
2022-06-21 19:53:07 +0800

    Update test.cc

--- anything/test.cc
+++ anything/test.cc
@@ -1,43 +1,14 @@
 #include <cstdio>
-#include <functional>
-#include <vector>
-#include <queue>
-using namespace std;
+#include <iostream>
 
-const int maxn = 500 + 5;
-vector<int> edge[maxn];
-vector<int> res;
-priority_queue<int, vector<int>, greater<int>> q;
-int ind[maxn];
+class A {
+public:
+    constexpr A() = default;
+    constexpr ~A() = default;
+};
 
 int main() {
-    int n, m, u, v;
-    scanf("%d%d", &n, &m);
-    for(int i = 0; i < m; i++) {
-        scanf("%d%d", &u, &v);
-        edge[u].push_back(v);
-        ind[v]++;
-    }
-    for(int i = 0; i < n; i++) {
-        if(ind[i] == 0) {
-            q.push(i);
-        }
-    }
-    while(!q.empty()) {
-        int p = q.top(); q.pop();
-        res.push_back(p);
-        for(auto it: edge[p]) {
-            ind[it]--;
-            if(ind[it] == 0) q.push(it);
-        }
-    }
-    if(res.size() == n) {
-        for(int i = 0; i < res.size(); i++) {
-            printf("%d ", res[i]);
-        }
-        puts("");
-    }else {
-        puts("false");
-    }
-    return 0;
+    int a;
+    scanf("%d", &a);
+    constexpr int b = a;
 }
