814121d8e05b682795f5822775cb07662fb6c209
typr7 <blame773@gmail.com>
2024-10-14 16:31:15 +0800

    Delete folder optix

--- optix/samples/src/GlContext.cpp
+++
@@ -1,8 +0,0 @@
-#include "GlContext.h"
-
-#include "Util.h"
-
-GlContext::GlContext(const std::string& window_title, int32_t output_width, int32_t output_height)
-{
-    m_window = createGlfwWindow(window_title, output_width, output_height);
-}
